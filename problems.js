11c223ff1129a174311394c408335b02bb7c55ea
hernanjkd <hernanjkd@gmail.com>
2019-07-23 19:58:16 -0400

    solved problem

--- problems.js
+++ problems.js
@@ -5,9 +5,9 @@ let div = document.querySelector(".display");
 
 let cards = [
      1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
-    41, 42, 43, 44, 45, 46, 47, 48, 49, 40, 51, 52, 53,
-    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73
+    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+    27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+    40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52
 ];
 
 let max = cards.length;
@@ -68,8 +68,18 @@ function BubbleSort(arr) {
             let change = false;
             // If the current number is larger than the following
             // swap them
-            if (arr[j] > arr[j+1]) {  
-                swap(arr, j, j+1);
+            if (arr[j] > arr[j+1]) {   
+
+                /**************************
+                 *  I can't use the swap function instead of what's exactly in the swap
+                 *  But the swap function works in the SelectorSort
+                 **************************/
+
+                 swap(arr, arr[j], arr[j+1]);
+
+                // let temp = arr[i];
+                // arr[i] = arr[j];
+                // arr[j] = temp;
                 change = true;
             }
             // Set the wall.. if the last 2 items are already in place,
@@ -80,7 +90,6 @@ function BubbleSort(arr) {
     }
     return arr;
 }
-
 
 
 
@@ -110,54 +119,3 @@ function swap(arr, i, j) {
     arr[j] = temp;
 }
 
-/************************************************* */
-
-function ran(length) {
-    return Math.floor(Math.random() * length);
-}
-
-const cardHTML = (value, suit) => {
-    return `
-    <span class="border p-2">${value} ${suit}</span>
-`}
-
-
-
-let log = [];
-
-function bubbleSort(array) {
-    for(wall=array.length-1; wall>0; wall--) {
-        for(x=0;x<wall;x++) {
-                if(
-                    array[x].suit > array[x+1].suit ||
-                    array[x].suit === array[x+1].suit &&
-                    array[x].value > array[x+1].value
-                ) {
-                    let aux = array[x];
-                    array[x]=array[x+1];
-                    array[x+1] = aux;
-                    let copyArr = array.slice();
-                    log.push(copyArr)
-                }
-        }
-    }
-}
-
-let cards = {
-    suits: [1,2,3,4],
-    value:[1,2,3,4,5,6,7,8,9,10,11,12,13]
-}
-
-let arr = []
-
-for (let i=0; i<20; i++)
-    arr.push({
-        suit: cards.suits[ran(4)],
-        value: cards.value[ran(13)]
-    })
-
-bubbleSort(arr)
-
-document.querySelector("#display").innerHTML = arr.map(e => {
-    return cardHTML(e.value, e.suit);
-}).join("");
