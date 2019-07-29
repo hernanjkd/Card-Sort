b1cf5092fb2c04e8ac8cac6219ce2dd26b657a48
hernanjkd <hernanjkd@gmail.com>
2019-07-29 13:48:04 -0400

    not connecting to js file

--- sort.js
+++ sort.js
@@ -1,110 +1,106 @@
 
 
-document.querySelector(".display").innerHTML = "helloooo";
 
-alert();
+function BubbleSort(arr) {
 
+    let len = arr.length;
 
-// function BubbleSort(arr) {
+    for (i = 0; i < arr.length; i++) {
+        for (j = 0; j < len - 1; j++) {
 
-//     let len = arr.length;
-
-//     for (i = 0; i < arr.length; i++) {
-//         for (j = 0; j < len - 1; j++) {
-
-//             let change = false;
-//             // If the current number is larger than the following
-//             // swap them
-//             if (arr[j] > arr[j+1]) {  
-//                 swap(arr, j, j+1);
-//                 change = true;
-//             }
-//             // Set the wall.. if the last 2 items are already in place,
-//             // don't check for them
-//             if (j === len-2 && !change)
-//                 len -= 2;
-//         }
-//     }
-//     return arr;
-// }
+            let change = false;
+            // If the current number is larger than the following
+            // swap them
+            if (arr[j] > arr[j+1]) {  
+                swap(arr, j, j+1);
+                change = true;
+            }
+            // Set the wall.. if the last 2 items are already in place,
+            // don't check for them
+            if (j === len-2 && !change)
+                len -= 2;
+        }
+    }
+    return arr;
+}
 
 
 
 
-// function SelectionSort(arr) {
-//     let len = arr.length;
-//     let min;
+function SelectionSort(arr) {
+    let len = arr.length;
+    let min;
 
-//     for (i = 0; i < len; i++) {
-//         min = i;
+    for (i = 0; i < len; i++) {
+        min = i;
 
-//         // Check the rest of the array
-//         for (j = i + 1; j < len; j++)
-//             if (arr[j] < arr[min])
-//                 min = j;
+        // Check the rest of the array
+        for (j = i + 1; j < len; j++)
+            if (arr[j] < arr[min])
+                min = j;
 
-//         if (i != min)
-//             swap(arr, i, min);
-//     }
-//     return arr;
-// }
-
-
-
-// function swap(arr, i, j) {
-//     let temp = arr[i];
-//     arr[i] = arr[j];
-//     arr[j] = temp;
-// }
-
-
-// function ran(length) {
-//     return Math.floor(Math.random() * length);
-// }
-
-// const cardHTML = (value, suit) => {
-//     return `
-//     <span class="border p-2">${value} ${suit}</span>
-// `}
+        if (i != min)
+            swap(arr, i, min);
+    }
+    return arr;
+}
 
 
 
-// let log = [];
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
 
-// function bubbleSort(array) {
-//     for(wall=array.length-1; wall>0; wall--) {
-//         for(x=0;x<wall;x++) {
-//                 if(
-//                     array[x].suit > array[x+1].suit ||
-//                     array[x].suit === array[x+1].suit &&
-//                     array[x].value > array[x+1].value
-//                 ) {
-//                     let aux = array[x];
-//                     array[x]=array[x+1];
-//                     array[x+1] = aux;
-//                     let copyArr = array.slice();
-//                     log.push(copyArr)
-//                 }
-//         }
-//     }
-// }
 
-// let cards = {
-//     suits: [1,2,3,4],
-//     value:[1,2,3,4,5,6,7,8,9,10,11,12,13]
-// }
+function ran(length) {
+    return Math.floor(Math.random() * length);
+}
 
-// let arr = []
+const cardHTML = (value, suit) => {
+    return `
+    <span class="border p-2">${value} ${suit}</span>
+`}
 
-// for (let i=0; i<20; i++)
-//     arr.push({
-//         suit: cards.suits[ran(4)],
-//         value: cards.value[ran(13)]
-//     })
 
-// bubbleSort(arr)
 
-// document.querySelector("#display").innerHTML = arr.map(e => {
-//     return cardHTML(e.value, e.suit);
-// }).join("");
+let log = [];
+
+function bubbleSort(array) {
+    for(wall=array.length-1; wall>0; wall--) {
+        for(x=0;x<wall;x++) {
+                if(
+                    array[x].suit > array[x+1].suit ||
+                    array[x].suit === array[x+1].suit &&
+                    array[x].value > array[x+1].value
+                ) {
+                    let aux = array[x];
+                    array[x]=array[x+1];
+                    array[x+1] = aux;
+                    let copyArr = array.slice();
+                    log.push(copyArr)
+                }
+        }
+    }
+}
+
+let cards = {
+    suits: [1,2,3,4],
+    value:[1,2,3,4,5,6,7,8,9,10,11,12,13]
+}
+
+let arr = []
+
+for (let i=0; i<20; i++)
+    arr.push({
+        suit: cards.suits[ran(4)],
+        value: cards.value[ran(13)]
+    })
+
+bubbleSort(arr)
+
+document.querySelector("#display").innerHTML = arr.map(e => {
+    return cardHTML(e.value, e.suit);
+}).join("");
 
